59276021ec4eb9dfef3eb3985af50fe09c8ab72b
alpa <makaveli1986@hotmail.de>
2024-01-10 15:51:14 +0100

    hero

---
+++ floh.store/src/components/button.component.jsx
@@ -0,0 +1,9 @@
+export function ButtonComponent({ text, additionalclasses, spanning }) {
+  const classes = `${additionalclasses} group rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 shadow-xl ring-inset ring-gray-300  hover:bg-emerald ease-in duration-300`;
+  return (
+    <button type="button" className={classes}>
+      {text}
+      {spanning}
+    </button>
+  );
+}
