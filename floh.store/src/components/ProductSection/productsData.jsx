6222416173d3f755246fa5f18067ec190a2ee40e
hindawi1 <ezzulddin_hindawi@hotmail.com>
2024-01-11 11:44:13 +0100

    updated

--- floh.store/src/components/ProductSection/productsData.jsx
+++ floh.store/src/components/ProductSection/productsData.jsx
@@ -1,4 +1,4 @@
-﻿const products = [
+﻿export const products = [
   {
   
     id: 1,
@@ -58,4 +58,4 @@
   },
 ];
 
-export { products };
+
--- floh.store/src/components/header/hero.component.jsx
+++ floh.store/src/components/header/hero.component.jsx
@@ -4,7 +4,7 @@ export function HeroComponent() {
   return (
     <div className="pt-12">
       <div className=" bg-[url('/public/floh2.png')] bg-cover container mx-auto pt-10">
-        <div class="pl-20 pt-12 w-5/5 h-96 ">
+        <div className="pl-20 pt-12 w-5/5 h-96 ">
           <div className="bg-emerald/70 w-2/6 h-40 rounded-lg pt-5">
             <ButtonComponent text="Finde dein" spanning="FLOH" />
             <p className="pt-3 text-white">
