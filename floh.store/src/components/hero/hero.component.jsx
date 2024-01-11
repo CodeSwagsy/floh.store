43b1469e9fe5f6709749af3172cce31192b09a25
doerrenbaecher <m.doerrenbaecher@googlemail.com>
2024-01-11 13:44:26 +0100

    einheitliche componenten namen etc.

--- floh.store/src/components/hero/hero.component.jsx
+++ floh.store/src/components/hero/hero.component.jsx
@@ -1,11 +1,11 @@
-import { ButtonComponent } from "../button.component";
+import { ButtonComponent } from "../button.component.jsx";
 
 export function HeroComponent() {
   return (
     <div>
       <div className=" bg-[url('/public/floh2.png')] bg-cover   w-screen">
         <div className="pt-12  container mx-auto">
-          <div class="pl-20 pt-12 w-5/5 h-96  ">
+          <div className="pl-20 pt-12 w-5/5 h-96  ">
             <div className="bg-emerald/70 w-2/6 h-40 rounded-lg pt-5">
               <ButtonComponent text="Finde dein " spantxt="FLOH" />
               <p className="pt-3 text-white">
