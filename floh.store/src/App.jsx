fb44f1b9162168a9cac0424e7b90e4933b3f1f73
hindawi1 <ezzulddin_hindawi@hotmail.com>
2024-01-11 10:19:52 +0100

    products

--- floh.store/src/App.jsx
+++ floh.store/src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
+import React from 'react';
 import {HeaderComponent} from "./components/header/header.component.jsx";
+import {ProductSections} from "./components/ProductSection/ProductSections.jsx";
+import {products} from './components/ProductSection/productsData';
 
 function App() {
     return (
         <>
             <HeaderComponent/>
+            <ProductSections products={products} />
+            
         </>
     )
 }
