0f1bed90e338734fa00f7771d8965971d0bf3588
doerrenbaecher <m.doerrenbaecher@googlemail.com>
2024-01-11 14:28:11 +0100

    edited

--- floh.store/src/components/productSection/productsection.component.jsx
+++ floh.store/src/components/productSection/productsection.component.jsx
@@ -3,31 +3,19 @@ import {ProductComponent} from "./product.component.jsx";
 
 export const ProductSectionComponent = ({ products }) => {
   const maxVisibleProducts = 5;
-
-
-
-  // Add event handlers to change overflow property on hover
-  const handleMouseEnter = () => {
-    sectionStyle.overflowX = 'auto';
-  };
-
-  const handleMouseLeave = () => {
-    sectionStyle.overflowX = 'hidden';
-  };
-
   return (
-    <div
-      className="flex flex-col items-center p-[20px] bg-emerald overflow-x-hidden mt-[80px]"
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
-    >
-      <h2 className="text-2xl mb-[10px]">New Products</h2>
-      <div className="flex gap-2.5 py-2.5">
-        {products.slice(0, maxVisibleProducts).map((product) => (
-          <ProductComponent key={product.id} product={product} />
-        ))}
+      <div className="flex flex-col items-center p-4 bg-emerald overflow-x-hidden mt-8 md:p-8 lg:p-12 xl:p-16 2xl:p-20">
+        <h2 className="text-2xl mb-4 md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl">New Products</h2>
+        <div className="flex flex-wrap justify-center gap-2.5 py-2.5">
+          {products.map((product, index) => (
+              <ProductComponent
+                  key={product.id}
+                  product={product}
+                  className={`lg:w-1/5 xl:w-1/5 2xl:w-1/5 ${index >= maxVisibleProducts ? 'hidden md:block' : ''}`}
+              />
+          ))}
+        </div>
       </div>
-    </div>
   );
 };
 
