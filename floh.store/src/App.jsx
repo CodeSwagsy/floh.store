d02cd32f1b2a4cfeab8b777cd7012db659766d2e
doerrenbaecher <m.doerrenbaecher@googlemail.com>
2024-01-11 10:16:26 +0100

    edit

--- floh.store/src/App.jsx
+++ floh.store/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import {HeaderComponent} from "./components/header/header.component.jsx";
+import {HeroComponent} from "./components/header/hero.component.jsx";
 
 function App() {
     return (
         <>
             <HeaderComponent/>
+            <HeroComponent/>
         </>
     )
 }
