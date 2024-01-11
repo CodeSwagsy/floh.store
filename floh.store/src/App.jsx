6e5265163f0a3e87dad7403a87c986c30d84838c
hindawi1 <ezzulddin_hindawi@hotmail.com>
2024-01-11 13:51:27 +0100

    updated

--- floh.store/src/App.jsx
+++ floh.store/src/App.jsx
@@ -1,12 +1,19 @@
-import "./App.css";
-import { HeaderComponent } from "./components/header/header.component.jsx";
+import './App.css'
+import React from 'react';
+import {HeaderComponent} from "./components/header/header.component.jsx";
+import {ProductSections} from "./components/ProductSection/ProductSections.jsx";
+import {products} from './components/ProductSection/productsData';
+import {HeroComponent} from "./components/header/hero.component.jsx";
+
 
 function App() {
-  return (
-    <>
-      <HeaderComponent />
-    </>
-  );
+    return (
+        <>
+            <HeaderComponent/>            
+            <HeroComponent/>
+            <ProductSections products={products} />
+        </>
+    )
 }
 
-export default App;
+export default App
--- floh.store/src/components/ProductSection/ProductSections.jsx
+++ floh.store/src/components/ProductSection/ProductSections.jsx
@@ -1,33 +1,21 @@
-﻿
-import {Product} from "./Product.jsx";
+﻿import React from 'react';
+import { Product } from "./Product.jsx";
 
 export const ProductSections = ({ products }) => {
   const maxVisibleProducts = 5;
 
-
-
-  // Add event handlers to change overflow property on hover
-  const handleMouseEnter = () => {
-    sectionStyle.overflowX = 'auto';
-  };
-
-  const handleMouseLeave = () => {
-    sectionStyle.overflowX = 'hidden';
-  };
-
   return (
-    <div
-      className="flex flex-col items-center p-[20px] bg-emerald overflow-x-hidden mt-[80px]"
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
-    >
-      <h2 className="text-2xl mb-[10px]">New Products</h2>
-      <div className="flex gap-2.5 py-2.5">
-        {products.slice(0, maxVisibleProducts).map((product) => (
-          <Product key={product.id} product={product} />
+    <div className="flex flex-col items-center p-4 bg-emerald overflow-x-hidden mt-8 md:p-8 lg:p-12 xl:p-16 2xl:p-20">
+      <h2 className="text-2xl mb-4 md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl">New Products</h2>
+      <div className="flex flex-wrap gap-2.5 py-2.5 justify-center">
+        {products.map((product, index) => (
+          <Product
+            key={product.id}
+            product={product}
+            className={`lg:w-1/5 xl:w-1/5 2xl:w-1/5 ${index >= maxVisibleProducts ? 'hidden md:block' : ''}`}
+          />
         ))}
       </div>
     </div>
   );
 };
-
