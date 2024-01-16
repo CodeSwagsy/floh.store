cd936529417c75efa66400b0d42fdcf317b6d1c0
alpa <makaveli1986@hotmail.de>
2024-01-16 17:09:11 +0100

    updateReg

--- floh.store/src/components/register/register.component.jsx
+++ floh.store/src/components/register/register.component.jsx
@@ -3,128 +3,192 @@ import { ButtonComponent } from "../hero/button.component";
 export function RegisterComponent() {
   return (
     <div className="flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
-      <section className="bg-white ">
-        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-          <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0  border-0">
-            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-              <h2 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
-                Erstelle dein Konto
-              </h2>
-              <form className="space-y-4 md:space-y-6" action="#">
-                <div>
-                  <label
-                    for="email"
-                    className="block mb-2 text-sm font-medium text-gray-900 "
-                  >
-                    Deine Email
-                  </label>
-                  <input
-                    type="email"
-                    name="email"
-                    id="email"
-                    className="p-2.5 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    placeholder="Email@adresse.com"
-                    required=""
-                  />
-                </div>
-                <div>
-                  <label
-                    for="passwort"
-                    className="block mb-2 text-sm font-medium text-gray-900 "
-                  >
-                    Passwort
-                  </label>
-                  <div className="mt-2">
-                    <input
-                      id="password"
-                      name="password"
-                      type="password"
-                      placeholder=" ••••••••"
-                      autoComplete="current-password"
-                      required
-                      className="p-2.5 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    />
-                  </div>
-                </div>
-                <div>
-                  <label
-                    for="Bestätige Passwort"
-                    className="block mb-2 text-sm font-medium text-black "
-                  >
-                    Bestätige Passwort
-                  </label>
-                  <input
-                    type="confirm-password"
-                    name="confirm-password"
-                    id="confirm-password"
-                    placeholder="••••••••"
-                    className="p-2.5 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    required=""
-                  />
-                  <div class="relative max-w-sm pt-6">
-                    <div class="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
-                      <svg
-                        class="w-4 h-4 text-gray-500 dark:text-gray-400"
-                        aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="currentColor"
-                        viewBox="0 0 20 20"
-                      >
-                        <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
-                      </svg>
-                    </div>
-                    <input
-                      datepicker
-                      type="text"
-                      class=" border border-gray-300 text-gray-600 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 dark:bg-white dark:border-gray-300 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                      placeholder="Geboren am"
-                    />
-                  </div>
-                </div>
-                <div class="flex items-start">
-                  <div class="flex items-center h-5">
-                    <input
-                      id="terms"
-                      aria-describedby="terms"
-                      type="checkbox"
-                      class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-                      required=""
-                    />
-                  </div>
-
-                  <div className="ml-3 text-sm">
-                    <label
-                      for="terms"
-                      className="font-light text-black dark:text-gray-600"
-                    >
-                      Ich Akzeptiere{" "}
-                      <a
-                        className="font-semibold  text-emerald hover:text-black underline underline-offset-4 ease-in duration-300"
-                        href="#"
-                      >
-                        Allgemeine Geschäftsbedingungen
-                      </a>
-                    </label>
-                  </div>
-                </div>
-
-                <div className="flex w-full justify-center rounded-md">
-                  <ButtonComponent text="Erstelle Konto" />
-                </div>
-                <p className="text-sm font-light  dark:text-gray-600">
-                  Du hast bereits ein Konto?{" "}
-                  <a
-                    href="#"
-                    className="font-semibold   text-emerald hover:text-black underline underline-offset-4 ease-in duration-300"
-                  >
-                    Anmelden
-                  </a>
-                </p>
-              </form>
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
+        <div className="bg-white px-6 py-12 shadow sm:rounded-lg sm:px-12">
+          <h2 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900 pb-10">
+            Registrieren
+          </h2>
+          <div className="w-24">
+            <label
+              htmlFor="gender"
+              className="block  text-sm font-medium leading-6 text-gray-900"
+            ></label>
+            <select
+              id="gender"
+              name="gender"
+              placeholder="Anrede"
+              className="  block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-emerald  focus:ring-2 focus:ring-inset focus:ring-emerald sm:text-sm sm:leading-6 "
+            >
+              <option value="Herr">Herr</option>
+              <option value="Frau">Frau</option>
+              <option value="KeineAngabe">Möchte ich nicht angeben</option>
+            </select>
+          </div>
+          <form className="space-y-6" action="#" method="POST">
+            <div className="flex space-x-6">
+              <div className="w-1/2">
+                <label
+                  htmlFor="grid-first-name"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Vorname
+                </label>
+                <input
+                  id="grid-first-name"
+                  name="grid-first-name"
+                  type="text"
+                  placeholder="Vorname"
+                  className="p-2.5 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-emerald placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+              <div className="w-1/2">
+                <label
+                  htmlFor="grid-last-name"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Nachname
+                </label>
+                <input
+                  id="grid-last-name"
+                  name="grid-last-name"
+                  type="text"
+                  placeholder="Nachname"
+                  className="p-2.5 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-emerald placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            <div>
+              <label
+                htmlFor="grid-street"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Telefon Nummer
+              </label>
+              <input
+                id="number"
+                name="number"
+                type="number"
+                placeholder="Telefon Nummer"
+                className="p-2.5 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-emerald placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="grid-street"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Email
+              </label>
+              <input
+                id="email"
+                name="email"
+                type="email"
+                placeholder="Email@adresse.com"
+                className="p-2.5 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-emerald placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+            <div className="flex space-x-6">
+              <div className="w-1/2">
+                <label
+                  htmlFor="grid-city"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Stadt
+                </label>
+                <input
+                  id="grid-city"
+                  name="grid-city"
+                  type="text"
+                  placeholder="Stadt"
+                  className="p-2.5 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-emerald placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+              <div className="w-1/2">
+                <label
+                  htmlFor="grid-zip"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Postleitzahl
+                </label>
+                <input
+                  id="grid-zip"
+                  name="grid-zip"
+                  type="text"
+                  placeholder="Postleitzahl"
+                  className="p-2.5 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-emerald placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            <div>
+              <label
+                htmlFor="grid-street"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Straße
+              </label>
+              <input
+                id="grid-street"
+                name="grid-street"
+                type="text"
+                placeholder="Straße"
+                className="p-2.5 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-emerald placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Passwort
+              </label>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                placeholder=" ••••••••"
+                className="p-2.5 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-emerald placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="password-confirm"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Passwort bestätigen
+              </label>
+              <input
+                id="password-confirm"
+                name="password-confirm"
+                type="password"
+                placeholder=" ••••••••"
+                className="p-2.5 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-emerald placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+            <div className="flex justify-center">
+              <ButtonComponent text="Registrieren" />
+            </div>
+          </form>
+          <div>
+            <div className="relative mt-10">
+              <div
+                className="absolute inset-0 flex items-center"
+                aria-hidden="true"
+              >
+                <div className="w-full border-t border-gray-200" />
+              </div>
             </div>
           </div>
         </div>
-      </section>
+        <p className="mt-10 text-center text-sm text-gray-500">
+          Bist du schon Mitglied?{" "}
+          <a
+            href="#"
+            className="font-semibold  text-emerald hover:text-black underline underline-offset-4 ease-in duration-300"
+          >
+            Melde dich mit deinem Konto an!
+          </a>
+        </p>
+      </div>
     </div>
   );
 }
