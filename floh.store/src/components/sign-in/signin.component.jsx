1a1c534da74aa11122f345c6400d1abcd6907509
alpa <makaveli1986@hotmail.de>
2024-01-16 17:09:11 +0100

    updateReg

--- floh.store/src/components/sign-in/signin.component.jsx
+++ floh.store/src/components/sign-in/signin.component.jsx
@@ -24,7 +24,7 @@ export function SigninComponent() {
                   autoComplete="email"
                   required
                   placeholder="Email@adresse.com"
-                  className="p-2.5 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className="p-2.5 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-emerald placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
@@ -43,7 +43,7 @@ export function SigninComponent() {
                   placeholder=" ••••••••"
                   autoComplete="current-password"
                   required
-                  className="p-2.5 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className="p-2.5 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-emerald placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
