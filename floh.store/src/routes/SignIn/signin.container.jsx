3400f8340fb42252afc49c184b92dbccee0dfbff
doerrenbaecher <m.doerrenbaecher@googlemail.com>
2024-01-16 11:58:44 +0100

    added signin container

--- floh.store/src/routes/SignIn/signin.container.jsx
+++ floh.store/src/routes/SignIn/signin.container.jsx
@@ -1,11 +1,12 @@
 import {HeaderComponent} from "../../components/header/header.component.jsx";
 import {FooterComponent} from "../../components/footer/footer.component.jsx";
+import {SigninComponent} from "../../components/sign-in/signin.component.jsx";
 
 export function SignInContainer() {
     return (
         <>
             <HeaderComponent/>
-            {/*<SignInComponent>*/}
+            <SigninComponent/>
             <FooterComponent/>
         </>
     )
