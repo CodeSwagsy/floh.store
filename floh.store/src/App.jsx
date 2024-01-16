777357d35890e1dd0586006b6be9331ae9abafc2
alpa <makaveli1986@hotmail.de>
2024-01-16 11:32:25 +0100

    sign-in

--- floh.store/src/App.jsx
+++ floh.store/src/App.jsx
@@ -1,17 +1,18 @@
 import "./App.css";
-import {HeaderComponent} from "./components/header/header.component.jsx";
-import {HeroComponent} from "./components/hero/hero.component.jsx";
-import {products} from "./components/productSection/product.data.jsx";
-import {ProductSectionComponent} from "./components/productSection/productsection.component.jsx";
-
+import { HeaderComponent } from "./components/header/header.component.jsx";
+import { HeroComponent } from "./components/hero/hero.component.jsx";
+import { products } from "./components/productSection/product.data.jsx";
+import { ProductSectionComponent } from "./components/productSection/productsection.component.jsx";
+import { SigninComponent } from "./components/sign-in/signin.component.jsx";
 function App() {
-    return (
-        <>
-            <HeaderComponent/>
-            <HeroComponent/>
-            <ProductSectionComponent products={products}/>
-        </>
-    );
+  return (
+    <>
+      <HeaderComponent />
+      <HeroComponent />
+      <ProductSectionComponent products={products} />
+      <SigninComponent />
+    </>
+  );
 }
 
 export default App;
--- floh.store/src/components/header/hero.component.jsx
+++ floh.store/src/components/header/hero.component.jsx
@@ -1,11 +1,11 @@
-import { ButtonComponent } from "./button.component.jsx";
+import { ButtonComponent } from "../hero/button.component";
 
 export function HeroComponent() {
   return (
     <div>
       <div className=" bg-[url('/public/floh2.png')] bg-cover   w-screen">
         <div className="pt-12  container mx-auto">
-          <div className="pl-20 pt-12 w-5/5 h-96  ">
+          <div class="pl-20 pt-12 w-5/5 h-96  ">
             <div className="bg-emerald/70 w-2/6 h-40 rounded-lg pt-5">
               <ButtonComponent text="Finde dein " spantxt="FLOH" />
               <p className="pt-3 text-white">
