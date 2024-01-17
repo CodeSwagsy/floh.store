536093137ee472132c4e23b52ada6d3bba7c2ccd
alpa <makaveli1986@hotmail.de>
2024-01-17 14:50:47 +0100

    update

--- floh.store/src/routes/Register/register.container.jsx
+++ floh.store/src/routes/Register/register.container.jsx
@@ -1,12 +1,13 @@
-import {HeaderComponent} from "../../components/header/header.component.jsx";
-import {FooterComponent} from "../../components/footer/footer.component.jsx";
+import { HeaderComponent } from "../../components/header/header.component.jsx";
+import { FooterComponent } from "../../components/footer/footer.component.jsx";
+import { RegisterComponent } from "../../components/register/register.component.jsx";
 
 export function RegisterContainer() {
-    return (
-        <>
-            <HeaderComponent/>
-            {/*<RegisterComponent>*/}
-            <FooterComponent/>
-        </>
-    )
+  return (
+    <>
+      <HeaderComponent />
+      <RegisterComponent />
+      <FooterComponent />
+    </>
+  );
 }
