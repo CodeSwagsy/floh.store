993b3fef5b6950ef952c63efa50f4da229082e31
doerrenbaecher <m.doerrenbaecher@googlemail.com>
2024-01-18 15:52:39 +0100

    added singleproductpage & added link function to gallery

--- floh.store/src/components/productSection/product.component.jsx
+++ floh.store/src/components/productSection/product.component.jsx
@@ -1,14 +1,12 @@
 ﻿import React, { useState } from 'react';
+import {Link} from "react-router-dom";
 
 export const ProductComponent = ({ product, style }) => {
     const { name, description, price, image } = product;
 
     const [isHovered, setIsHovered] = useState(false);
 
-    const handleViewDetails = () => {
-        // Add logic to navigate to a detailed product page or show a modal
-        alert(`View details for ${name}`);
-    };
+
 
     return (
         <div className={`bg-gray-100 p-4 border rounded-lg hover:shadow-md transition duration-300 ${style}`}>
@@ -19,14 +17,14 @@ export const ProductComponent = ({ product, style }) => {
                 <h3 className="text-xl md:text-2xl lg:text-3xl mb-2">{name}</h3>
                 <p className="text-sm md:text-base lg:text-lg mb-2">{description}</p>
                 <p className="font-bold text-base md:text-lg lg:text-xl mb-2">{`Price: $${price}`}</p>
-                <button
+                <Link to={`/products/${product._id}`}
                     className={`bg-green-500 text-white px-5 py-2 rounded mt-4 ${isHovered ? 'bg-green-600' : 'bg-gray-800'}`}
-                    onClick={handleViewDetails}
+
                     onMouseEnter={() => setIsHovered(true)}
                     onMouseLeave={() => setIsHovered(false)}
                 >
                     View Details
-                </button>
+                </Link>
             </div>
         </div>
     );
---
+++ floh.store/src/routes/SingleProduct/singleproduct.container.jsx
@@ -0,0 +1,14 @@
+import {HeaderComponent} from "../../components/header/header.component.jsx";
+import {FooterComponent} from "../../components/footer/footer.component.jsx";
+import {SingleProductComponent} from "../../components/singleProduct/singleproduct.component.jsx";
+
+
+export function SingleProductContainer() {
+    return (
+        <>
+            <HeaderComponent/>
+            <SingleProductComponent/>
+            <FooterComponent/>
+        </>
+    )
+}
