4b838fc7192b9c063ff80b7b81be6c2ee87a0693
doerrenbaecher <m.doerrenbaecher@googlemail.com>
2024-01-19 11:50:33 +0100

    edited productpage & added settings component

---
+++ floh.store/src/components/profile/settings.component.jsx
@@ -0,0 +1,9 @@
+export function SettingsComponent() {
+    return (
+        <>
+            <div className="container mx-auto lg:mt-12">
+                <h1 className="text-3xl font-bold">Profileinstellungen</h1>
+            </div>
+        </>
+    )
+}
