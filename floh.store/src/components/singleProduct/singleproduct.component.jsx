22f252bea9dd352c2b81ee0383821bb08ac06e54
doerrenbaecher <m.doerrenbaecher@googlemail.com>
2024-01-19 11:50:33 +0100

    edited productpage & added settings component

--- floh.store/src/components/singleProduct/singleproduct.component.jsx
+++ floh.store/src/components/singleProduct/singleproduct.component.jsx
@@ -1,8 +1,9 @@
 import {useEffect, useState} from "react";
 import {useParams} from "react-router";
+import {LinkButtonComponent} from "../header/button.component.jsx";
 
 export function SingleProductComponent() {
-    const { id } = useParams();
+    const {id} = useParams();
     const [product, setProduct] = useState({});
     const [formattedDate, setFormattedDate] = useState(null);
 
@@ -40,25 +41,49 @@ export function SingleProductComponent() {
 
     return (
         <>
-            <div className="container mx-auto">
-                <div className="flex py-4">
-                    <h2 className="font-semibold underline">{product.category}</h2>
-                    <h2 className="no-underline">&nbsp; / {product.title}</h2>
+            <div className="container mx-auto lg:mt-12">
+                <div className="flex pb-4">
+                    <h2 className="lg:text-2xl font-semibold underline">{product.category}</h2>
+                    <h2 className="lg:text-2xl no-underline">&nbsp; / {product.title}</h2>
                 </div>
-                <div className="flex flex-col lg:flex-row justify-between">
-                    <div className="max-lg:px-2 lg:w-5/12 pb-4 lg:mb-4 flex justify-center items-center">
-                        <img src={product.images ? product.images[0] : " "} alt="TESTBILD"/>
+                <div className="flex flex-col lg:flex-row justify-between lg:pb-4">
+                    <div className="max-lg:px-2 lg:w-5/12 flex ">
+                        <img src={product.images ? product.images[0] : " "} alt={product.title} className="max-h-96"/>
                     </div>
-                    <div className="max-lg:w-full w-5/12 max-lg:mb-4 max-lg:px-2">
-                        <h1 className="text-lg font-bold">{product.title}</h1>
-                        <p className="text-lg font-bold">{product.price} €</p>
-                        <div className="flex flex-row gap-2">
-                            <h2 className="">{product.location ? product.location.zip : " "}</h2>
-                            <p className="">{product.location ? product.location.city : " "}</p>
+                    <div className="max-lg:mb-2 max-lg:px-2 flex flex-col lg:justify-center lg:w-5/12 max-lg:mt-4">
+                        <div className="w-1/2">
+                            <h1 className="text-lg font-bold">{product.title}</h1>
+                            <p className="text-lg font-bold">{product.price} €</p>
+                            <div className="flex flex-row gap-2">
+                                <h2 className="">{product.location ? product.location.zip : " "}</h2>
+                                <p className="">{product.location ? product.location.city : " "}</p>
+                            </div>
                         </div>
-                        <div className="flex flex-col items-start">
-                            <button className="">Angebot machen</button>
-                            <button>Zur Merkliste hinzufügen {"<3"}</button>
+
+                        <div className="flex flex-col max-lg:flex-row gap-2 grow items-center md:items-start justify-center my-2">
+                            <LinkButtonComponent text="Angebot machen"
+                                                 additionalClasses="bg-emerald hover:bg-jet transition-all w-1/2 lg:px-2 py-1 lg:py-1.5 xl:text-xl"/>
+                            <LinkButtonComponent text="Zur Merkliste hinzufügen"
+                                                 additionalClasses="bg-emerald hover:bg-jet transition-all w-1/2 lg:px-2 py-1 lg:py-1.5 xl:text-xl"/>
+
+                        </div>
+
+                        <div className="">
+                            <h2 className="font-bold mb-2">Verkäufer:</h2>
+                            <div className="flex flex-col">
+                                <div className="flex gap-4">
+                                    <h3 className="font-semibold w-6/12 lg:w-5/12">Nutzername</h3>
+                                    <p>NUTZERNAME LOGIK</p>
+                                </div>
+                                <div className="flex gap-4">
+                                    <h3 className="font-semibold w-6/12 lg:w-5/12">Durchschnittliche Bewertung</h3>
+                                    <p>BEWERTUNG LOGIK</p>
+                                </div>
+                                <div className="flex gap-4">
+                                    <h3 className="font-semibold w-6/12 lg:w-5/12">Mitglied seit</h3>
+                                    <p>CREATED LOGIK</p>
+                                </div>
+                            </div>
                         </div>
                     </div>
 
@@ -67,15 +92,15 @@ export function SingleProductComponent() {
                 <div className="border-y border-gray-300 py-4 max-lg:px-2">
                     <h2 className="font-bold mb-2">Produktdetails:</h2>
                     <div className="flex gap-4">
-                        <h3 className="font-semibold max-lg:w-3/12 w-2/12">Preis</h3>
+                        <h3 className="font-semibold w-6/12 lg:w-3/12">Preis</h3>
                         <p>{product.price} €</p>
                     </div>
                     <div className="flex gap-4">
-                        <h3 className="font-semibold max-lg:w-3/12 w-2/12">Zustand</h3>
+                        <h3 className="font-semibold w-6/12 lg:w-3/12">Zustand</h3>
                         <p>{product.condition}</p>
                     </div>
                     <div className="flex gap-4">
-                        <h3 className="font-semibold max-lg:w-3/12 w-2/12">Inseriert seit:</h3>
+                        <h3 className="font-semibold w-6/12 lg:w-3/12">Inseriert seit:</h3>
                         <p>{formattedDate}</p>
                     </div>
                 </div>
