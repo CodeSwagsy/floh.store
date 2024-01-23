875a15c0ff7dcbac31fc06d72eef5c5f00987566
hindawi1 <ezzulddin_hindawi@hotmail.com>
2024-01-23 14:34:25 +0100

    Product

--- floh.store/src/components/ProductsPage/AllProductsPage.jsx
+++ floh.store/src/components/ProductsPage/AllProductsPage.jsx
@@ -19,19 +19,23 @@ export const ProductCard = ({ product }) => {
       }`}
     >
       <div className="flex justify-center mb-3">
-        <img src={images[0]} alt={title} className="w-40 h-40 rounded" />
+        <img
+          src={images[0]}
+          alt={title}
+          className="w-full h-64 object-cover rounded"
+        />
       </div>
       <div className="text-center">
-        <h3 className="text-lg md:text-xl lg:text-2xl xl:text-3xl mb-2">
+        <h3 className="text-lg md:text-xl lg:text-2xl xl:text-3xl mb-2 truncate">
           {title}
         </h3>
         <p className="text-sm md:text-base lg:text-lg xl:text-xl mb-2">{`Category: ${category}`}</p>
         <p className="text-sm md:text-base lg:text-lg xl:text-xl mb-2">{`Condition: ${condition}`}</p>
         <p className="font-bold text-base md:text-lg lg:text-xl xl:text-2xl mb-2">{`Price: $${price}`}</p>
-        <p className="text-sm md:text-base lg:text-lg xl:text-xl mb-2">{`Description: ${description}`}</p>
-        <p className="text-sm md:text-base lg:text-lg xl:text-xl mb-2">{`Location: ${location.city}, ${location.zip}`}</p>
+        <p className="text-sm md:text-base lg:text-lg xl:text-xl mb-2 overflow-hidden h-16">{`Description: ${description}`}</p>
+        <p className="text-sm md:text-base lg:text-lg xl:text-xl mb-2 overflow-hidden h-10">{`Location: ${location.city}, ${location.zip}`}</p>
         <button
-          className={`bg-green-500 text-white px-5 py-2 rounded mt-4 ${
+          className={`bg-green-500 text-white px-3 py-1 rounded mt-4 ${
             isHovered ? "bg-green-600" : "bg-gray-800"
           }`}
           onClick={handleViewDetails}
@@ -72,7 +76,7 @@ export const AllProductsPage = () => {
   }, []);
 
   return (
-    <div className="container mx-auto my-8 mt-16">
+    <div className=" container mx-auto my-8 mt-16">
       <h2 className="text-3xl font-bold mb-4">All Products</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
         {products.map((product) => (
