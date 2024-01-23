88cc0c19875998bf9adfdd23fd02bc7ad75b453c
doerrenbaecher <m.doerrenbaecher@googlemail.com>
2024-01-23 14:51:34 +0100

    nein

--- floh.store/src/components/ProductsPage/AllProductsPage.jsx
+++ floh.store/src/components/ProductsPage/AllProductsPage.jsx
@@ -1,19 +1,14 @@
-﻿// AllProductsPage.jsx
-import React, { useEffect, useState } from "react";
-import {Link} from "react-router-dom";
+﻿import {Link} from "react-router-dom";
 
 export const ProductCard = ({ product }) => {
   const { title, category, condition, price, description, location, images } =
     product;
 
-  const [isHovered, setIsHovered] = useState(false);
 
 
   return (
     <div
-      className={`bg-gray-100 p-4 border rounded-lg hover:shadow-md transition duration-300 mb-4 flex flex-col justify-between ${
-        isHovered ? "shadow-md" : ""
-      }`}
+      className={`bg-gray-100 p-4 border rounded-lg hover:shadow-md transition duration-300 mb-4 flex flex-col justify-between`}
     >
 
       <div className="flex justify-center mb-3">
@@ -32,14 +27,10 @@ export const ProductCard = ({ product }) => {
         <p className="font-bold text-base md:text-lg lg:text-xl xl:text-2xl mb-2">{`Price: $${price}`}</p>
         <p className="text-sm md:text-base lg:text-lg xl:text-xl mb-2 overflow-hidden h-16">{`Description: ${description}`}</p>
         <p className="text-sm md:text-base lg:text-lg xl:text-xl mb-2 overflow-hidden h-10">{`Location: ${location.city}, ${location.zip}`}</p>
-        <button
-          className={`bg-green-500 text-white px-3 py-1 rounded mt-4 ${
+        <Link
+          className="bg-green-500 text-white px-3 py-1 rounded mt-4"
 
-            isHovered ? "bg-green-600" : "bg-gray-800"
-          }`}
-          onMouseEnter={() => setIsHovered(true)}
-          onMouseLeave={() => setIsHovered(false)}
-        >
+         to={}>
           View Details
         </Link>
       </div>
