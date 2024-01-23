c76fd05026f59950baa63c61ef45ff496b8e6eb4
doerrenbaecher <m.doerrenbaecher@googlemail.com>
2024-01-23 10:49:41 +0100

    Edit

--- floh.store/src/App.jsx
+++ floh.store/src/App.jsx
@@ -1,45 +1,47 @@
 import "./App.css";
 import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
+    createBrowserRouter,
+    createRoutesFromElements,
+    Route,
+    RouterProvider,
 } from "react-router-dom";
-import { IndexContainer } from "./routes/Index/index.container.jsx";
-import { ProfileContainer } from "./routes/Profile/profile.container.jsx";
-import { SettingsContainer } from "./routes/ProfileSettings/settings.container.jsx";
-import { GalleryContainer } from "./routes/Gallery/gallery.container.jsx";
-import { SignInContainer } from "./routes/SignIn/signin.container.jsx";
-import { RegisterContainer } from "./routes/Register/register.container.jsx";
-import { OwnProductsContainer } from "./routes/OwnProducts/ownproducts.container.jsx";
-import { FavoriteProductsContainer } from "./routes/FavoriteProducts/favoriteproducts.container.jsx";
-import { AddProductContainer } from "./routes/AddProduct/addproducts.container.jsx";
-import { AboutContainer } from "./routes/About/about.container.jsx";
-import { RandomProductContainer } from "./routes/RandomProduct/randomproduct.container.jsx";
-import { KontaktContainer } from "./routes/Kontakt/kontakt.container.jsx";
-import { DatenschutzContainer } from "./routes/Datenschutz/datenschutz.container.jsx";
-import { ImpressumContainer } from "./routes/Impressum/impressum.container.jsx";
-import { ForgotPasswordContainer } from "./routes/ForgotPassword/forgotpassword.container.jsx";
-import { NewPasswordContainer } from "./routes/NewPassword/newpassword.container.jsx";
+import {IndexContainer} from "./routes/Index/index.container.jsx";
+import {ProfileContainer} from "./routes/Profile/profile.container.jsx";
+import {SettingsContainer} from "./routes/ProfileSettings/settings.container.jsx";
+import {GalleryContainer} from "./routes/Gallery/gallery.container.jsx";
+import {SignInContainer} from "./routes/SignIn/signin.container.jsx";
+import {RegisterContainer} from "./routes/Register/register.container.jsx";
+import {OwnProductsContainer} from "./routes/OwnProducts/ownproducts.container.jsx";
+import {FavoriteProductsContainer} from "./routes/FavoriteProducts/favoriteproducts.container.jsx";
+import {AddProductContainer} from "./routes/AddProduct/addproducts.container.jsx";
+import {AboutContainer} from "./routes/About/about.container.jsx";
+import {RandomProductContainer} from "./routes/RandomProduct/randomproduct.container.jsx";
+import {KontaktContainer} from "./routes/Kontakt/kontakt.container.jsx";
+import {DatenschutzContainer} from "./routes/Datenschutz/datenschutz.container.jsx";
+import {ImpressumContainer} from "./routes/Impressum/impressum.container.jsx";
+import {ForgotPasswordContainer} from "./routes/ForgotPassword/forgotpassword.container.jsx";
+import {NewPasswordContainer} from "./routes/NewPassword/newpassword.container.jsx";
+import {SingleProductContainer} from "./routes/SingleProduct/singleproduct.container.jsx";
+import {DataProvider} from "./context/signin.context.jsx";
 
 const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/">
-      <Route index element={<IndexContainer />} />
-      <Route path="/home" element={<IndexContainer />} />
-      <Route path="/start" element={<IndexContainer />} />
-      <Route path="/about" element={<AboutContainer />} />
-      <Route path="/kontakt" element={<KontaktContainer />} />
-      <Route path="/datenschutz" element={<DatenschutzContainer />} />
-      <Route path="/impressum" element={<ImpressumContainer />} />
-      <Route path="/profile" element={<ProfileContainer />} />
-      <Route path="/profile/settings" element={<SettingsContainer />} />
-      <Route path="/profile/signin" element={<SignInContainer />} />
-      <Route
-        path="/profile/forgotpassword"
-        element={<ForgotPasswordContainer />}
-      />
-      <Route path="/profile/newpassword" element={<NewPasswordContainer />} />
+    createRoutesFromElements(
+        <Route path="/">
+            <Route index element={<IndexContainer/>}/>
+            <Route path="/home" element={<IndexContainer/>}/>
+            <Route path="/start" element={<IndexContainer/>}/>
+            <Route path="/about" element={<AboutContainer/>}/>
+            <Route path="/kontakt" element={<KontaktContainer/>}/>
+            <Route path="/datenschutz" element={<DatenschutzContainer/>}/>
+            <Route path="/impressum" element={<ImpressumContainer/>}/>
+            <Route path="/profile" element={<ProfileContainer/>}/>
+            <Route path="/profile/settings/:id" element={<SettingsContainer/>}/>
+            <Route path="/profile/signin" element={<SignInContainer/>}/>
+            <Route
+                path="/profile/forgotpassword"
+                element={<ForgotPasswordContainer/>}
+            />
+            <Route path="/profile/newpassword" element={<NewPasswordContainer/>}/>
 
             <Route path="/profile/register" element={<RegisterContainer/>}/>
             <Route path="/profile/ownproducts" element={<OwnProductsContainer/>}/>
@@ -57,11 +59,13 @@ const router = createBrowserRouter(
 );
 
 function App() {
-  return (
-    <>
-      <RouterProvider router={router} />
-    </>
-  );
+    return (
+        <>
+            <DataProvider>
+                <RouterProvider router={router}/>
+            </DataProvider>
+        </>
+    );
 }
 
 export default App;
