78fb96a2260c360ec342e9a0f50fbc720cc2f786
doerrenbaecher <m.doerrenbaecher@googlemail.com>
2024-01-24 13:58:36 +0100

    added rating function

--- floh.store/src/components/productSection/productsection.component.jsx
+++ floh.store/src/components/productSection/productsection.component.jsx
@@ -9,7 +9,7 @@ export const ProductSectionComponent = () => {
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-        const response = await fetch("https://api.floh.store/product/all", {
+        const response = await fetch(`${import.meta.env.VITE_API}/product/all`, {
           method: "GET",
           headers: {
             "Content-Type": "application/json",
