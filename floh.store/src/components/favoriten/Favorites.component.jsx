7357e032aebbac3516b437a727680164056e1b07
doerrenbaecher <m.doerrenbaecher@googlemail.com>
2024-01-25 15:51:35 +0100

    edit

--- floh.store/src/components/favoriten/Favorites.component.jsx
+++ floh.store/src/components/favoriten/Favorites.component.jsx
@@ -7,6 +7,36 @@ export const FavoriteComponent = () => {
     const uid = localStorage.getItem("responseData");
     const [favorites, setFavorites] = useState({})
 
+    const handleRemoveFavorite = async (product) => {
+        try {
+            if (!product) {
+                console.error("Product is undefined or null.");
+                return;
+            }
+
+            const response = await fetch(
+                `${import.meta.env.VITE_API}/user/delete/favorites/${product._id}`,
+                {
+                    method: "DELETE",
+                    credentials: "include",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({product}),
+                }
+            );
+
+            const data = await response.json();
+            if (data.code === 200) {
+                console.log("FAVORITE ENTFERNT")
+            } else {
+                console.error("Error removing from favorites:", data.message);
+            }
+        } catch (error) {
+            console.error("Error removing from favorites:", error);
+        }
+    };
+
     useEffect(() => {
             const fetchProducts = async () => {
                 try {
@@ -53,7 +83,7 @@ export const FavoriteComponent = () => {
 
             fetchProducts();
             fetchFavoriteProducts();
-        }, []
+        }, [handleRemoveFavorite]
     )
     ;
 
@@ -61,14 +91,13 @@ export const FavoriteComponent = () => {
         favorites.includes(product._id)
     );
 
-
     return (
         <div className=" container mx-auto my-8 mt-16">
             <h2 className="text-3xl font-bold mb-4">Meine Favoriten</h2>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
                 {filteredProducts.length > 0 ? (
                     filteredProducts.map((product) => (
-                        <ProductCard key={product._id} product={product}/>
+                        <ProductCard key={product._id} product={product} favoriteText="Aus Favoriten entfernen" onAddToFavorites={handleRemoveFavorite}/>
                     ))
                 ) : (
                     <p>Keine Produkte vorhanden.</p>
