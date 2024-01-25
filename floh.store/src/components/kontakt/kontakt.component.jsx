e331e596910b607125343e231c0b2ea161be925b
alpa <makaveli1986@hotmail.de>
2024-01-25 10:46:35 +0100

    update

---
+++ floh.store/src/components/kontakt/kontakt.component.jsx
@@ -0,0 +1,13 @@
+export function KontaktComponent() {
+  return (
+    <>
+      <div className="flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
+        <div className="container mx-auto">
+          <div className="mt-10 ">
+            <div className="bg-white px-6 py-12 shadow sm:rounded-lg sm:px-12"></div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
