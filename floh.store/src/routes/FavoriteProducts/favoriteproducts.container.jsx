ccd13d6885d98ce217c24d477ca3e93cb7c5a8a8
doerrenbaecher <m.doerrenbaecher@googlemail.com>
2024-01-25 11:39:08 +0100

    edit

--- floh.store/src/routes/FavoriteProducts/favoriteproducts.container.jsx
+++ floh.store/src/routes/FavoriteProducts/favoriteproducts.container.jsx
@@ -1,6 +1,6 @@
 import { HeaderComponent } from "../../components/header/header.component.jsx";
 import { FooterComponent } from "../../components/footer/footer.component.jsx";
-import { FavoriteComponent } from "../../components/favoriten/favoriten.component.jsx";
+import { FavoriteComponent } from "../../components/favoriten/Favorites.component.jsx";
 
 export function FavoriteProductsContainer() {
   return (
