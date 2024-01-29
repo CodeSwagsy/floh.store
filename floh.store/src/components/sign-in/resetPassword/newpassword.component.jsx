16bd62ad97552db5df635bdebeeb2cda9f02d255
hindawi1 <ezzulddin_hindawi@hotmail.com>
2024-01-29 09:05:37 +0100

    Products

---
+++ floh.store/src/components/sign-in/resetPassword/newpassword.component.jsx
@@ -0,0 +1,77 @@
+import { Link } from "react-router-dom";
+import { ButtonComponent } from "../../hero/button.component";
+
+export function NewPasswordComponent() {
+  return (
+    <div className="flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
+        <div className="bg-white px-6 py-12 shadow sm:rounded-lg sm:px-12">
+          <h2 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900 pb-10">
+            Gib dein neues Passwort ein!
+          </h2>
+
+          <form className="space-y-6" action="#" method="POST">
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Neues Passwort
+              </label>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="password"
+                  required
+                  placeholder=" ••••••••"
+                  className="p-2.5 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-emerald placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-emerald sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            <div>
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Bestätige neues Passwort
+              </label>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  placeholder=" ••••••••"
+                  autoComplete="current-password"
+                  required
+                  className="p-2.5 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-emerald placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-emerald sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div className="pb-8  flex w-full justify-center rounded-md">
+              <ButtonComponent
+                spantxt="Bestätigen"
+                size="large"
+                height="height"
+              />
+            </div>
+          </form>
+          <div>
+            <div className=" pb-0 w-full border-t border-gray-200"> </div>
+            <p className="mt-10 text-center text-sm text-gray-500">
+              Noch kein mitglied?{" "}
+              <Link
+                to="/profile/register"
+                className="font-semibold  hover:text-black text-emerald hover:text-black underline underline-offset-4 ease-in duration-300"
+              >
+                Jetzt Registrieren!
+              </Link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
