e65027b2c04f4ff2fd857f63cdf8fd6e38f8b8f4
doerrenbaecher <m.doerrenbaecher@googlemail.com>
2024-01-29 11:28:16 +0100

    edited random anzeige component

--- floh.store/src/components/RandomProduct/randomproduct.component.jsx
+++ floh.store/src/components/RandomProduct/randomproduct.component.jsx
@@ -7,37 +7,17 @@ export const RandomProductComponent = () => {
     useEffect(() => {
         const fetchRandomProduct = async () => {
             try {
-                // Fetch all products
                 const allProductsResponse = await fetch(`${import.meta.env.VITE_API}/product/all`, {
                     method: 'GET',
                     headers: {
                         'Content-Type': 'application/json',
                     },
                 });
-
                 const allProductsData = await allProductsResponse.json();
-
                 if (allProductsData.code === 200 && allProductsData.products.length > 0) {
                     const randomIndex = Math.floor(Math.random() * allProductsData.products.length);
                     const randomProduct = allProductsData.products[randomIndex];
-                    const singleProductResponse = await fetch(
-                        `${import.meta.env.VITE_API}/product/id/${randomProduct._id}`,
-                        {
-                            method: 'GET',
-                            credentials: 'include',
-                            headers: {
-                                'Content-Type': 'application/json',
-                            },
-                        }
-                    );
-
-                    const singleProductData = await singleProductResponse.json();
-
-                    if (singleProductData.code === 200) {
-                        setRandomProduct(randomProduct);
-                    } else {
-                        console.error('Error fetching individual product:', singleProductData.message);
-                    }
+                    setRandomProduct(randomProduct);
                 } else {
                     console.error('Error fetching all products:', allProductsData.message);
                 }
@@ -52,6 +32,7 @@ export const RandomProductComponent = () => {
     if (randomProduct) {
         navigate(`/products/${randomProduct._id}`)
     }
+
     return (
         <>
         </>
