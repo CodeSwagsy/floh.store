51bae4bdfeb21e9641087656eddb202387b0cf33
doerrenbaecher <m.doerrenbaecher@googlemail.com>
2024-01-31 15:47:14 +0100

    added password recover / password apply logic

--- floh.store/src/components/hero/button.component.jsx
+++ floh.store/src/components/hero/button.component.jsx
@@ -9,12 +9,12 @@ export function ButtonComponent({
   const buttonWidthClass = size === "large" ? "w-full" : "w-16";
   const buttonHeightClass = height === "height" ? "h-12" : "h-1";
 
-  const classes = `${additionalclasses} group rounded-md bg-white ${buttonWidthClass} ${buttonHeightClass} px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset shadow-2xl shadow-black ring-gray-300 hover:bg-emerald ease-in duration-300`;
+  const classes = `${additionalclasses} group rounded-md bg-white ${buttonWidthClass} ${buttonHeightClass} px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset shadow-2xl shadow-black ring-gray-300 hover:bg-emerald transition-all`;
 
   return (
     <button type={buttonType} className={classes}>
       {text}
-      <span className="group-hover:text-white ease-in duration-300">
+      <span className="group-hover:text-white transition-all">
         {spantxt}
       </span>
     </button>
