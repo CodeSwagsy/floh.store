a06fe30ff05a1f76f398ec71648e6f9b4084633e
Aloblanko <125378394+Aloblanko@users.noreply.github.com>
2024-02-01 14:53:30 +0100

    de

--- floh.store/src/App.jsx
+++ floh.store/src/App.jsx
@@ -1,17 +1,19 @@
 import "./App.css";
-import {HeaderComponent} from "./components/header/header.component.jsx";
-import {HeroComponent} from "./components/hero/hero.component.jsx";
-import {products} from "./components/productSection/product.data.jsx";
-import {ProductSectionComponent} from "./components/productSection/productsection.component.jsx";
+import { HeaderComponent } from "./components/header/header.component.jsx";
+import { HeroComponent } from "./components/hero/hero.component.jsx";
+import Loader from "./components/loader/loader.component.jsx";
+import { products } from "./components/productSection/product.data.jsx";
+import { ProductSectionComponent } from "./components/productSection/productsection.component.jsx";
 
 function App() {
-    return (
-        <>
-            <HeaderComponent/>
-            <HeroComponent/>
-            <ProductSectionComponent products={products}/>
-        </>
-    );
+  return (
+    <>
+    
+      <HeaderComponent />
+      <HeroComponent />
+      <ProductSectionComponent products={products} />
+    </>
+  );
 }
 
 export default App;
