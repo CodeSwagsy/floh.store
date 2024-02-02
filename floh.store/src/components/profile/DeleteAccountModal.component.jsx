0f5642b92fc4ef42fe5195b8ded849102efbf733
doerrenbaecher <m.doerrenbaecher@googlemail.com>
2024-02-02 12:16:58 +0100

    added settings logic & modals

---
+++ floh.store/src/components/profile/DeleteAccountModal.component.jsx
@@ -0,0 +1,153 @@
+import {Fragment, useEffect, useRef, useState} from 'react'
+import {Dialog, Transition} from '@headlessui/react'
+import {ExclamationTriangleIcon, XMarkIcon} from '@heroicons/react/24/outline'
+import {useNavigate} from "react-router-dom";
+import {useData} from "../../context/signin.context.jsx";
+
+export function DeleteAccountModalComponent({closeModal}) {
+    const [open, setOpen] = useState(true);
+    const dialogRef = useRef();
+    const navigate = useNavigate()
+    const {userData, updateUserData, login, updateLogin} = useData();
+
+
+    const handleCloseModal = () => {
+        setOpen(false);
+        closeModal();
+    };
+
+    const handleClickOutside = (event) => {
+        if (dialogRef.current && !dialogRef.current.contains(event.target)) {
+            handleCloseModal();
+        }
+    };
+
+    const handleKeyDown = (event) => {
+        if (event.key === 'Escape') {
+            handleCloseModal();
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener('mousedown', handleClickOutside);
+        document.addEventListener('keydown', handleKeyDown);
+
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+            document.removeEventListener('keydown', handleKeyDown);
+        };
+    }, []);
+
+    const handleDeleteAccount = async (e) => {
+        e.preventDefault()
+        try {
+            const response = await fetch(
+                `${import.meta.env.VITE_API}/user/delete/account`,
+                {
+                    method: "DELETE",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    credentials: "include",
+                    body: JSON.stringify({})
+                }
+            )
+
+            const data = await response.json();
+            if (response.status === 200) {
+                console.log("Account wurde gelöscht")
+                updateLogin(false);
+                updateUserData(null);
+                localStorage.removeItem("uid");
+                localStorage.removeItem("loginData");
+                navigate("/")
+            } else {
+                console.error("Account konnte nicht gelöscht werden:", data)
+            }
+
+        } catch (error) {
+            console.error("Fehler bei der löschung des Accounts:", error)
+        }
+    }
+
+    return (
+        <Transition.Root show={open} as={Fragment}>
+            <Dialog as="div" className="relative z-10" onClose={closeModal} ref={dialogRef}>
+                <Transition.Child
+                    as={Fragment}
+                    enter="ease-out duration-300"
+                    enterFrom="opacity-0"
+                    enterTo="opacity-100"
+                    leave="ease-in duration-200"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                >
+                    <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"/>
+                </Transition.Child>
+
+                <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+                    <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                        <Transition.Child
+                            as={Fragment}
+                            enter="ease-out duration-300"
+                            enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                            enterTo="opacity-100 translate-y-0 sm:scale-100"
+                            leave="ease-in duration-200"
+                            leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                            leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                        >
+                            <Dialog.Panel
+                                className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
+                                <div className="absolute right-0 top-0 hidden pr-4 pt-4 sm:block">
+                                    <button
+                                        type="button"
+                                        className="rounded-md bg-white text-gray-400 hover:text-emerald focus:outline-none focus:ring-2 focus:ring-emerald focus:ring-offset-2"
+                                        onClick={handleCloseModal}
+                                    >
+                                        <span className="sr-only">Close</span>
+                                        <XMarkIcon className="h-6 w-6" aria-hidden="true"/>
+                                    </button>
+                                </div>
+                                <div className="sm:flex sm:items-start">
+                                    <div
+                                        className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                                        <ExclamationTriangleIcon className="h-6 w-6 text-red-600" aria-hidden="true"/>
+                                    </div>
+                                    <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                                        <Dialog.Title as="h3"
+                                                      className="text-base font-semibold leading-6 text-gray-900">
+                                            Account löschen
+                                        </Dialog.Title>
+                                        <div className="mt-2">
+                                            <p className="text-sm text-gray-500">
+                                                Sicher, dass du dein Konto löschen möchtest? Alle deine Daten werden
+                                                dauerhaft von unseren Servern entfernt. Diese Aktion kann nicht
+                                                rückgängig gemacht werden.
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
+                                    <button
+                                        type="button"
+                                        className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
+                                        onClick={handleDeleteAccount}
+                                    >
+                                        Account löschen
+                                    </button>
+                                    <button
+                                        type="button"
+                                        className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 hover:text-emerald hover:ring-emerald sm:mt-0 sm:w-auto"
+                                        onClick={handleCloseModal}
+                                    >
+                                        Abbrechen
+                                    </button>
+                                </div>
+                            </Dialog.Panel>
+                        </Transition.Child>
+                    </div>
+                </div>
+            </Dialog>
+        </Transition.Root>
+    )
+}
