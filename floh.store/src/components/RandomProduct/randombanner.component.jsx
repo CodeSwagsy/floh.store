12c53b100204731a605b0fc6da46e8d3099f2aa4
Aloblanko <125378394+Aloblanko@users.noreply.github.com>
2024-02-09 07:21:52 +0100

    lots of corrections

--- floh.store/src/components/RandomProduct/randombanner.component.jsx
+++ floh.store/src/components/RandomProduct/randombanner.component.jsx
@@ -22,12 +22,17 @@ export const RandomBannerComponent = () => {
           allProductsData.products.length > 0
         ) {
           const randomProductsArray = [];
-          for (let i = 0; i < 8; i++) {
+
+          while (randomProductsArray.length < 8) {
             const randomIndex = Math.floor(
               Math.random() * allProductsData.products.length
             );
-            randomProductsArray.push(allProductsData.products[randomIndex]);
+            const randomProduct = allProductsData.products[randomIndex];
+            if (!randomProductsArray.some(product => product._id === randomProduct._id)) {
+              randomProductsArray.push(randomProduct);
+            }
           }
+      
          
           setRandomProduct(randomProductsArray);
         } else {
@@ -50,20 +55,28 @@ export const RandomBannerComponent = () => {
   return (
     <div className="container mx-auto ">
         <h2 className="text-2xl lg:text-4xl my-4 lg:mt-12 lg:mb-8 text-emerald font-bold">Entdecke die Vielfalt</h2>
-      <div className=" bg-platinum p-3 max-w-full mx-auto mt-10 grid grid-cols-8 rounded-md  ">
+      <div className=" bg-whitesmoke px-1 max-w-full mx-auto mt-10 grid grid-cols-8 ">
         {randomProduct.map((randomProduct, index) => (
+            //  console.log(randomProduct.images),
           <div
             key={index}
-            className=" items-end group h-64 rounded-md flex-shrink-0 place-content: center  focus:bg-emerald mx-1 "
+            className="group h-64  flex-shrink-0 place-content: center  
+             mx-1 bg-whitesmoke "
           >
-            {randomProduct.images && randomProduct.images.length > 0 && (
+      
+          
+            
+                
               <img
-                className=" h-32 w-auto object-cover rounded-t-md group "
-                src={randomProduct.images[0]}
+                className="  h-32 w-auto mx-auto object-cover rounded-t-md group border-b-4 border-solid border-b-emerald"
+                src={randomProduct.images && randomProduct.images.length > 0 ? randomProduct.images[0] : 'https://picsum.photos/200'}
                 alt={randomProduct.title}
+                
               />
-            )}
-            <div className="space-y-2 text-wrap group-hover:bg-emerald border-solid border border-jet border-1 bg-whitesmoke  rounded-b-md shadow-md h-32 text-center p-2">
+             
+             
+            
+            <div className="space-y-2 text-wrap group-hover:bg-emerald border-solid border border-jet border-1 bg-whitesmoke  rounded-b-md shadow-md h-32 text-center p-2 my-auto">
 
               <p className=" text-jet font-medium leading-5 group-hover:text-whitesmoke">
                 {randomProduct.title}
@@ -81,3 +94,5 @@ export const RandomBannerComponent = () => {
     </div>
   );
 };
+
+
