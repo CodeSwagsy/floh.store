28b1d693d7d24cee02ac715af57da672236e4ebc
Aloblanko <125378394+Aloblanko@users.noreply.github.com>
2024-02-14 05:09:51 +0100

    many little changes

--- floh.store/src/components/hero/hero.component.jsx
+++ floh.store/src/components/hero/hero.component.jsx
@@ -25,25 +25,28 @@ export function HeroComponent() {
 
   return (
     <div
-      className="rounded-lg bg-center bg-cover w-full h-[330px] lg:max-w-[1920px] lg:h-[600px] 2xl:h-[700px] flex justify-center items-end mx-auto max-lg:items-center"
+      className="rounded-lg bg-center bg-cover w-full h-[330px] lg:max-w-[1920px] lg:h-[600px] 2xl:h-[700px] flex items-center mx-auto max-lg:items-center"
       style={{ backgroundImage: `url(${getRandomImage()})` }}
     >
-      <div className="container flex max-lg:items-center max-lg:justify-center">
-        <div className="flex flex-col justify-evenly w-96 h-48 lg:h-56 bg-emerald/80 rounded-lg p-4 lg:mb-16">
-           <Link to="/products/random" className="w-full">
+      <div className="container flex max-lg:items-center max-lg:justify-center justify-center ">
+        <div className="flex flex-col justify-evenly w-96 h-48 lg:h-56 bg-emerald/70 rounded-lg p-4 lg:mb-16">
+           <Link to="/products/random" className="w-full flex justify-center">
             <ButtonComponent
               text="Finde deinen "
-              spantxt="FLOH"
+              spantxt="Floh"
               size="large"
               height="height"
+             
+              
             />
           </Link>
-          <p className="text-white text-center">
+          <p className="text-whitesmoke text-center">
             Entdecke einen der größten lokalen Marktplätze für Käufer und
             Verkäufer in deiner Nähe
           </p>
         </div>
       </div>
+      
     </div>
   );
 }
