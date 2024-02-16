95b471284c7481740ac19b8b744756c923315d03
doerrenbaecher <m.doerrenbaecher@googlemail.com>
2024-02-16 11:01:24 +0100

    edit

--- floh.store/src/components/hero/button.component.jsx
+++ floh.store/src/components/hero/button.component.jsx
@@ -12,7 +12,7 @@ export function ButtonComponent({
     const buttonWidthClass = size === "large" ? "w-3/5" : "w-16";
     const buttonHeightClass = height === "height" ? "h-12" : "h-1";
 
-    const classes = `${additionalclasses} inline-block group rounded-lg bg-whitesmoke ${buttonWidthClass} ${buttonHeightClass} px-3 py-2 text-base font-semibold text-jet ring-1 shadow-black ring-jet hover:bg-springgreen hover:ring-springgreen transition-all whitespace-nowrap`;
+    const classes = `${additionalclasses} inline-block group rounded-lg bg-whitesmoke ${buttonWidthClass} ${buttonHeightClass} px-3 py-2 text-base font-semibold text-jet ring-1 shadow-black ring-jet hover:bg-springgreen hover:text-jet hover:ring-springgreen transition-all whitespace-nowrap`;
 
     return (
         <button type={buttonType} className={classes}>
